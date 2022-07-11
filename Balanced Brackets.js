e3ea4e9ab0606d95fe998bce51ee2c1f2c5775fe
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-11 15:57:02 +0530

    Create Balanced Brackets.js

---
+++ Balanced Brackets.js
@@ -0,0 +1,83 @@
+function processData(input) {
+    //Enter your code here
+    
+    var res = '', tempStr;
+    
+    testArr = input.split('\n').slice(1);
+    
+    //console.log(testArr);
+    
+    main: for (var i = 0; i < testArr.length; i++) {
+    
+        var string = testArr[i];
+        tempStr = '';
+        
+        for (var j = 0; j < string.length; j++) {
+            
+            switch (string[j]) {
+                case ')' :
+                    if (tempStr && tempStr.slice(-1) === '(') {
+                        tempStr = tempStr.slice(0, -1);
+                        break;
+                    } else {
+                        no();
+                        continue main;
+                    };
+                case '}' :
+                    if (tempStr && tempStr.slice(-1) === '{') {
+                        tempStr = tempStr.slice(0, -1);
+                        break;
+                    } else {
+                        no();
+                        continue main;
+                    };
+                case ']' :
+                    if (tempStr && tempStr.slice(-1) === '[') {
+                        tempStr = tempStr.slice(0, -1);
+                        break;
+                    } else {
+                        no();
+                        continue main;
+                    };
+                default: tempStr += string[j];
+            }
+            
+            //console.log(tempStr);
+
+        };
+        
+        if (!tempStr) {
+            yes();
+        } else {
+            no();
+        }
+        
+    };
+    
+    
+    function yes() {
+        res += 'YES' + '\n';
+    }
+    
+    function no() {
+        res += 'NO' + '\n';
+    }
+    
+    console.log(res.trim());
+    
+    
+    
+    
+    
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
