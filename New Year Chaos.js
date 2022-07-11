5b5669fd54b6cd6c111d49e56edc7a5349f76371
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-11 14:49:16 +0530

    added to chatic

---
+++ New Year Chaos.js
@@ -0,0 +1,24 @@
+function minimumBribes(q) {
+    let count = 0;
+    
+   for (let i = q.length-1; i >=0; i--) {
+    
+     if( q[i] - (i+1) > 2){
+       console.log("Too chaotic");
+       return;
+     }
+ 
+     for (let j = Math.max(0, q[i] - 2); j < i; j++){
+      
+       if (q[j] > q[i]){
+         count++;
+ 
+       } 
+     }
+           
+   } 
+ 
+   console.log(count);
+ }
+
+ 
