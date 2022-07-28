f4a070778a1ea6c4721325ada62223e725b49d7f
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-28 21:06:52 +0530

    Create Taum and B'day.js

---
+++ Taum and B'day.js
@@ -0,0 +1,27 @@
+
+/*
+ * Complete the 'taumBday' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER b
+ *  2. INTEGER w
+ *  3. INTEGER bc
+ *  4. INTEGER wc
+ *  5. INTEGER z
+ */
+
+function taumBday(b, w, bc, wc, z) {
+    // Write your code here
+  const gifts = BigInt(b) + BigInt(w);
+
+    const whiteGifts = gifts * BigInt(wc) + (BigInt(b) * BigInt(z));
+    const blackGifts = gifts * BigInt(bc) + (BigInt(w) * BigInt(z));
+
+    let min = BigInt(b) * BigInt(bc) + BigInt(w) * BigInt(wc);
+
+    (blackGifts < min) && (min = blackGifts);
+    (whiteGifts < min) && (min = whiteGifts);
+
+    return min;
+}
