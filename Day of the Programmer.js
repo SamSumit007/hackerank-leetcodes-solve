ab90d193d03ae74ca4e790f9f51b96a08d503d58
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-30 19:35:11 +0530

    Create Day of the Programmer.js

---
+++ Day of the Programmer.js
@@ -0,0 +1,17 @@
+
+/*
+ * Complete the 'dayOfProgrammer' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts INTEGER year as parameter.
+ */
+
+function dayOfProgrammer(year) {
+    // Write your code here
+   if (year == 1918)
+        return "26.09.1918";
+    else if (((year <= 1917) && (year % 4 == 0)) || ((year % 400 == 0) || ((year % 4 == 0) & (year % 100 != 0))))
+        return "12.09." + year;
+    else
+        return "13.09." + year;
+}
