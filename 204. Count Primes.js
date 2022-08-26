5a610a37521cd196be30bc87e629783016cc9a12
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-08-26 19:20:39 +0530

    Create 204. Count Primes.js

---
+++ 204. Count Primes.js
@@ -0,0 +1,23 @@
+/**
+https://leetcode.com/problems/count-primes/
+be aware of TLE coz we need to pass 66 test cases in this sums
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    const isPrime = new Array(n).fill(1);
+    const primes = [];
+    for (let i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            primes.push(i);
+        }
+
+        for (let j = 0; j < primes.length && i * primes[j] < n; j++) {
+            isPrime[i * primes[j]] = 0;
+            if (i % primes[j] === 0) {
+                break;
+            }
+        }
+    }
+    return primes.length;
+};
